bd50d534d266af72b641ca05a0dac78805433c78
nidazhong <nidazhong17@163.com>
2025-12-13 17:44:06 +0800

    feat：二级分类

--- src/api/category.js
+++ src/api/category.js
@@ -4,4 +4,23 @@ export default {
   getTopCategory(id) {
     return http.get('/category',{params:{id}})
   },
+  getCategoryFilterAPI(id) {
+    return http.get('/category/sub/filter',{params:{id}})
+  },
+
+  /**
+   * @description: 获取导航数据
+   * @data {
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   }
+   * @return {*}
+   */
+// 获取二级分类商品列表
+  getSecondCategoryAPI (data){
+    return http.post('/category/goods/temporary',data);
+  }
+
 }
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Layout from '@/views/Layout/index.vue'
 import Login from '@/views/Login/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/Category/composables/SubCategory.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +18,11 @@ const router = createRouter({
         {
           path:'category/:id', // 不用加 "/"
           component: Category
-        }
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
+        },
       ]
 
     },
