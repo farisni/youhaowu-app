87f96f9efe1f5a55d885e26a51ee6e37e41293b8
nidazhong <nidazhong17@163.com>
2025-12-14 12:43:12 +0800

    feat：全局组件插件化配置

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+
+// 把compomens里的所有组件全局注册
+import ImageView from '@/components/ImageView/index.vue'
+import Sku from '@/components/Sku/index.vue'
+
+// const { install } = require("element-plus");
+
+// 通过组件的方式
+export const componentPlugin = {
+  install(app) {
+    // app的方法
+    // app.components('组件名称', 组件配置对象)
+    app.component('YhwImageView', ImageView)
+    app.component('YhwSku', Sku)
+  }
+}
